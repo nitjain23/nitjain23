6eb57863c9993d58c5dba2088fc342d20df745d5
nitjain23 <nitjain23@outlook.com>
2024-08-26 19:35:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnet_core_hello_world
+{
+    internal class Program
+    {
+        internal static string Name="Nitin Jain";
+        static void Main(string[] args)
+        {
+            LogMsg();
+        }
+
+        private async static Task LogMsg()
+        {
+            Console.WriteLine($"Hello {Name}. How are you dear.?");
+        }
+    }
+}
